7a7e9bc4e2fbeba7fa53b0cc0c0600fee7e05900
alexthehighiqidiot <alexthehighiqidiot@gmail.com>
2025-03-24 11:24:34 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "type a number " << std:endl;
+    std::cin >> a;
+    std::cout << "type a function" << endl;
+    std::cin >> f;
+    std::cout << "type another number" << std::endl
+    std::cin >> b;
+    x = double(a)
+    y = double(b)
+    if (f == "+") {
+        std::cout << x + y << std::endl;
+    }
+    else if (f == "-") {
+        std::cout << x - y << std::endl;
+    }
+    else if (f == "*") {
+        std::cout << x * y << std::endl;
+    }
+    else if (f == "/") {
+        std::cout << x / y << std::endl;
+    }
+    else {
+        std::cout << "invalid fuction" << std::endl;
+    }
+
+
+    return 0
+}
